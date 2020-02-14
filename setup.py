d603cad2175ff8820675f901fd1042cc1d9714ac
Raphael Lange <rlange@sec55-dynip-211.gfz-potsdam.de>
2020-02-14 12:18:54 +0100

    add setup file for packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name="orographic_precipitation",
+     version='0.1',
+     description="Linear Theory of Orographic Precipitation",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/rlange2/orographic-precipitation",
+     download_url="",
+     license = "MIT",
+     maintainer="Raphael Lange",
+     maintainer_email="raphael.lange@gfz-potsdam.de",
+     packages=setuptools.find_packages(),
+     python_requires=">=3.6",
+     install_requires=["matplotlib", "numpy"]
+ )
