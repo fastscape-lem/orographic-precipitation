9830ef30316aa38234e138a2253deef47269b5a9
rlange2 <raphael.lange@mail.de>
2020-05-19 10:27:46 +0200

    Introduce meaningful variables names
    Input variables names have been changed to (hopefully) become more
    intuitive. `attr` has been added to the list of imports to support
    custom metadata with the `attrs` argument. Lastly, to maintain
    consistency, the drainage process name has been changed.

--- orographic_precipitation/fastscape_ext.py
+++ orographic_precipitation/fastscape_ext.py
@@ -1,3 +1,4 @@
+import attr
 import numpy as np
 import xsimlab as xs
 
@@ -9,21 +10,19 @@ from .orographic_precipitation import compute_orographic_precip
 @xs.process
 class OrographicPrecipitation:
     """Computes orographic precipitation following Smith & Barstad (2004)
-
-    Returns precipitation rate in m/yr
     """
     # --- initial conditions
-    gamma = xs.variable(description="adiabatic lapse rate")
-    gamma_m = xs.variable(description="moist adiabatic lapse rate")
-    rhosref = xs.variable(description="reference density")
+    lapse_rate = xs.variable(description="adiabatic lapse rate")
+    lapse_rate_m = xs.variable(description="moist adiabatic lapse rate")
+    ref_density = xs.variable(description="reference density")
 
     # --- input variables
-    latitude = xs.variable(description="latitude")
-    p0 = xs.variable(description="background precipitation")
-    windspeed = xs.variable(description="windspeed")
-    winddir = xs.variable(description="wind direction")
-    tau_c = xs.variable(description="conversion time")
-    tau_f = xs.variable(description="fallout time")
+    lat = xs.variable(description="latitude")
+    precip_base = xs.variable(description="background precipitation")
+    wind_speed = xs.variable(description="windspeed")
+    wind_dir = xs.variable(description="wind direction")
+    conv_time = xs.variable(description="conversion time")
+    fall_time = xs.variable(description="fallout time")
     nm = xs.variable(description="moist stability frequency")
     hw = xs.variable(description="water vapor scale height")
     cw = xs.variable(description="uplift sensitivity", intent="out")
@@ -35,18 +34,21 @@ class OrographicPrecipitation:
     elevation = xs.foreign(SurfaceTopography, "elevation")
 
     # --- output variable
-    precip_rate = xs.variable(dims=("y", "x"), description="precipitation rate", intent="out")
+    precip_rate = xs.variable(
+        dims=("y", "x"), description="precipitation rate", intent="out", attrs={"units": "mm/h"}
+    )
 
     def _get_params(self):
-        return {"latitude" : self.latitude,
-                   "p0" : self.p0,
-                   "windspeed" : self.windspeed,
-                   "winddir" : self.winddir,
-                   "tau_c" : self.tau_c,
-                   "tau_f" : self.tau_f,
-                   "nm" : self.nm,
-                   "hw" : self.hw,
-                   "cw" : self.rhosref * self.gamma_m / self.gamma}
+        return {
+            "latitude" : self.lat,
+            "p0" : self.precip_base,
+            "windspeed" : self.wind_speed,
+            "winddir" : self.wind_dir,
+            "tau_c" : self.conv_time,
+            "tau_f" : self.fall_time,
+            "nm" : self.nm,
+            "hw" : self.hw,
+            "cw" : self.ref_density * self.lapse_rate_m / self.lapse_rate}
 
     def initialize(self):
         self._params = self._get_params()
@@ -58,13 +60,16 @@ class OrographicPrecipitation:
                                                 self.dx,
                                                 self.dy,
                                                 **self._params)
-        self.precip_rate = self.precip_rate[:] * 8.76
 
 
 @xs.process
-class Discharge(FlowAccumulator):
-    runoff = xs.foreign(OrographicPrecipitation, 'precip_rate', attrs={"units": "mm/h"})
-    discharge = xs.on_demand(dims=('y','x'), description='discharge from orographic precipitation', attrs={"units": "mm/h"})
+class OrographicDrainageDischarge(FlowAccumulator):
+    """Flowaccumulation including orographic precipitation
+    """
+    runoff = xs.foreign(OrographicPrecipitation, 'precip_rate')
+    discharge = xs.on_demand(
+        dims=('y','x'), description='discharge from orographic precipitation', attrs={"units": "mm/h"}
+    )
 
     def run_step(self):
         super().run_step()
@@ -79,5 +84,5 @@ class Discharge(FlowAccumulator):
 
 precip_model = basic_model.update_processes({
     'orographic': OrographicPrecipitation,
-    'drainage': Discharge
+    'drainage': OrographicDrainageDischarge
 })
